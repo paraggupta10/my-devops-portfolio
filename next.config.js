90afaffa1a28b422d1a91f4741edfceb322c1bc0
paraggupta <paraggupta@adobe.com>
2024-06-30 22:09:23 +0530

    Updated Next.js configuration for static exports

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 // next.config.js
 module.exports = {
     assetPrefix: process.env.NODE_ENV === 'production' ? '/my-devops-portfolio/' : '',
-    output: {
+    output: 'export',
+//    output: {
       // Set the directory where the exported files will be placed
-      dir: 'docs', // Replace 'docs' with your desired output directory
-    },
+//      dir: 'docs', // Replace 'docs' with your desired output directory
+//    },
 };
